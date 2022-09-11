6b41c7466a2f8d4d5914883d6a62c290bc4527ac
clboy <syl@clboy.cn>
2022-09-11 23:00:02 +0800

    add file-service-standard-name-move

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 
 
+## picgo-plugin-dynamic-object-name
+
+> picgo上传时根据路径匹配配置动态重命名对象名称插件
+>
+> 最近一次维护时间：2022.09.09
+
 
 
 ## docsify-decrypt-content-plugin
@@ -19,6 +25,14 @@
 > 油猴脚本
 
 [脚本列表](tampermonkey/README.md)
+
+
+
+## file-service-standard-name-move
+
+> 将文件按时间戳重名并移动到指定目录，最后替换其他地方的文本内容引用文件地址的路径
+>
+>  最近一次维护时间：2022.09.11
 
 
 
---
+++ file-service-standard-name-move/index.js
@@ -0,0 +1,101 @@
+/*
+ * @Author: clboy
+ * @Date: 2022-09-11 12:51:10
+ * @LastEditors: clboy
+ * @LastEditTime: 2022-09-11 20:03:27
+ * @Description: 笔记oss文件名称时间戳规范化目录迁移
+ * 
+ * Copyright (c) 2022 by clboy syl@clboy.cn, All Rights Reserved. 
+ */
+import fs from "fs";
+import path from "path";
+
+const replaceContentDir = "/media/clboy/Repository/workspace/openSource/myNote/docs";
+const bucketDir = "/home/clboy/Desktop/note-1259193042";
+const filterDir = ["/home/clboy/Desktop/note-1259193042/images/2022"];
+const targetPrefix = "images/2021";
+const matchContentFileExt = [".js", ".html", ".md", ".txt"];
+const domain = "https://cdn.tencentfs.clboy.cn";
+
+assertIsDirectory(replaceContentDir);
+assertIsDirectory(bucketDir);
+
+
+async function nameSupplier() {
+    await sleep(5);
+    let date = new Date();
+    date.setFullYear(2021);
+    return padStartZero(date.getFullYear())
+        + padStartZero(date.getMonth() + 1)
+        + padStartZero(date.getDate())
+        + padStartZero(date.getHours())
+        + padStartZero(date.getMinutes())
+        + padStartZero(date.getSeconds())
+        + padStartZero(date.getMilliseconds(), 3);
+};
+
+
+handle(bucketDir);
+
+
+const record = {};
+async function handle(dirPath) {
+    const dirContent = fs.readdirSync(dirPath, { withFileTypes: true });
+    for (let dirent of dirContent) {
+        let filePath = path.join(dirPath, dirent.name)
+        if (dirent.isFile()) {
+            let newPath = path.join(bucketDir, targetPrefix, await nameSupplier()) + path.extname(dirent.name);
+            record[filePath] = newPath;
+            fs.writeFileSync('record.json', JSON.stringify(record, null, 4));
+            fs.renameSync(filePath, newPath);
+            //文件内容替换
+            replaceContentDirHandle(replaceContentDir,
+                domain + filePath.substring(bucketDir.length),
+                domain + newPath.substring(bucketDir.length));
+        } else if (dirent.isDirectory() && filterDir.indexOf(filePath) < 0) {
+            handle(filePath);
+        }
+    }
+}
+
+
+function replaceContentDirHandle(dirPath, searchText, replaceText) {
+    const dirContent = fs.readdirSync(dirPath, { withFileTypes: true });
+    for (let dirent of dirContent) {
+        let filePath = path.join(dirPath, dirent.name)
+        if (dirent.isFile() && matchContentFileExt.indexOf(path.extname(dirent.name))) {
+            let textContent = fs.readFileSync(filePath, { encoding: "utf-8" });
+            if (textContent.indexOf(searchText) >= 0) {
+                console.log(`替换文件：${filePath}`);
+                console.log(`\t${searchText} ===> ${replaceText}`);
+                fs.writeFileSync(filePath, replaceAll(searchText, replaceText, textContent))
+            }
+        } else if (dirent.isDirectory()) {
+            replaceContentDirHandle(filePath, searchText, replaceText);
+        }
+    }
+}
+
+
+function assertIsDirectory(path) {
+    const state = fs.statSync(path);
+    if (!state.isDirectory()) {
+        throw new Error(path + " 不是一个目录");
+    }
+}
+
+function padStartZero(number, length) {
+    if (isNaN(length)) {
+        length = 2;
+    }
+    return (number + '').padStart(length, '0');
+}
+
+function sleep(millis) {
+    return new Promise((resolve) => setTimeout(resolve, millis));
+}
+
+function replaceAll(find, replace, str) {
+    var find = find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    return str.replace(new RegExp(find, 'g'), replace);
+}
